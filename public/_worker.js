b5c0e40c82173afd0306ffaf195b796c9fa76fee
Greg Brimble <gbrimble@cloudflare.com>
2022-03-14 19:03:32 +0000

    debugger

--- public/_worker.js
+++ public/_worker.js
@@ -1,11 +1,63 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.searchParams.has("ct")) {
-      const response = await env.ASSETS.fetch(request);
-      return new Response(response.headers.get("content-type"));
+    let { pathname, origin, search } = new URL(request.url);
+
+    if (pathname !== "/") {
+      // Serve requests with trailing slashes
+      if (pathname.endsWith("/")) {
+        const assetUrlWithoutTrailingSlash = new URL(
+          pathname.slice(0, -1),
+          origin
+        );
+
+        const assetEntry = await env.ASSETS.fetch(
+          assetUrlWithoutTrailingSlash,
+          request
+        );
+
+        if (
+          assetEntry.status === 200 &&
+          assetEntry.headers.get("content-type").includes("text/html")
+        ) {
+          return assetEntry;
+        }
+      }
+      // Redirect requests without trailing slashes
+      else {
+        const assetUrlWithTrailingSlash = new URL(
+          `${pathname}/${search}`,
+          origin
+        );
+
+        const assetEntry = await env.ASSETS.fetch(
+          assetUrlWithTrailingSlash,
+          request
+        );
+
+        if (
+          assetEntry.status === 200 &&
+          assetEntry.headers.get("content-type").includes("text/html")
+        ) {
+          return new Response(
+            JSON.stringify({
+              url: assetUrlWithTrailingSlash.toString(),
+              status: assetEntry.status,
+              headers: [...assetEntry.headers.entries()],
+              body: await assetEntry.text(),
+            })
+          );
+          return new Response(null, {
+            // Temporary
+            status: 302,
+            headers: {
+              Location: `${pathname}/${search}`,
+            },
+          });
+        }
+      }
     }
 
-    return env.ASSETS.fetch(request);
+    // Fall back to asset server
+    return env.ASSETS.fetch(request.url, request);
   },
 };

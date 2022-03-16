50bea395a5449b21446119657caf7d2e68dc4caf
Greg Brimble <gbrimble@cloudflare.com>
2022-03-16 22:31:31 +0000

    test

---
+++ public/_worker copy.js
@@ -0,0 +1,35 @@
+export default {
+  async fetch(request, env) {
+    let { pathname, origin, search } = new URL(request.url);
+
+    if (pathname !== "/") {
+      if (!pathname.endsWith("/")) {
+        const assetUrlWithTrailingSlash = new URL(
+          `${pathname}/${search}`,
+          origin
+        );
+
+        const assetEntry = await env.ASSETS.fetch(
+          assetUrlWithTrailingSlash.toString(),
+          request
+        );
+
+        if (
+          assetEntry.status === 200 &&
+          assetEntry.headers.get("content-type").includes("text/html")
+        ) {
+          return new Response(null, {
+            // Temporary
+            status: 302,
+            headers: {
+              Location: `${pathname}/${search}`,
+            },
+          });
+        }
+      }
+    }
+
+    // Fall back to asset server
+    return env.ASSETS.fetch(request);
+  },
+};
--- public/_worker.js
+++ public/_worker.js
@@ -1,39 +1,5 @@
 export default {
   async fetch(request, env) {
-    let { pathname, origin, search } = new URL(request.url);
-
-    if (pathname !== "/") {
-      if (!pathname.endsWith("/")) {
-        const indexEtag = env.ASSETS.fetch(new URL("/", origin), request).then(
-          (response) => response.headers.get("etag")
-        );
-
-        const assetUrlWithTrailingSlash = new URL(
-          `${pathname}/${search}`,
-          origin
-        );
-
-        const assetEntry = await env.ASSETS.fetch(
-          assetUrlWithTrailingSlash.toString(),
-          request
-        );
-
-        if (
-          assetEntry.status === 200 &&
-          assetEntry.headers.get("content-type").includes("text/html") &&
-          (await indexEtag) !== assetEntry.headers.get("etag")
-        ) {
-          return new Response(null, {
-            // Temporary
-            status: 302,
-            headers: {
-              Location: `${pathname}/${search}`,
-            },
-          });
-        }
-      }
-    }
-
     // Fall back to asset server
     return env.ASSETS.fetch(request);
   },

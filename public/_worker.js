10e3ba8a276d43c67dc1f4bea3938737ff521fb4
Greg Brimble <gbrimble@cloudflare.com>
2022-03-16 23:44:49 +0000

    Done

--- public/_worker.js
+++ public/_worker.js
@@ -1,41 +1,37 @@
 export default {
   async fetch(request, env) {
-    try {
-      let { pathname, origin, search } = new URL(request.url);
+    let { pathname, origin, search } = new URL(request.url);
 
-      if (pathname !== "/") {
-        if (!pathname.endsWith("/")) {
-          const assetUrlWithTrailingSlash = new URL(
-            `${pathname}/${search}`,
-            origin
-          );
+    if (pathname !== "/") {
+      if (!pathname.endsWith("/")) {
+        const assetUrlWithTrailingSlash = new URL(
+          `${pathname}/${search}`,
+          origin
+        );
 
-          let assetRequest = new Request(assetUrlWithTrailingSlash, request);
-          assetRequest = new Request(assetRequest, {
+        let assetRequest = new Request(assetUrlWithTrailingSlash, request);
+        assetRequest = new Request(assetRequest, {
+          headers: {
+            ...Object.fromEntries(request.headers.entries()),
+            "cf-worker": "user-worker", // Deactivates SPA mode
+          },
+        });
+
+        const assetResponse = await env.ASSETS.fetch(assetRequest);
+
+        if (
+          assetResponse.status === 200 &&
+          assetResponse.headers.get("content-type").includes("text/html")
+        ) {
+          return new Response(null, {
+            // Temporary
+            status: 302,
             headers: {
-              ...Object.fromEntries(request.headers.entries()),
-              "cf-worker": "user-worker", // Deactivates SPA mode
+              Location: `${pathname}/${search}`,
             },
           });
-
-          const assetResponse = await env.ASSETS.fetch(assetRequest);
-
-          if (
-            assetResponse.status === 200 &&
-            assetResponse.headers.get("content-type").includes("text/html")
-          ) {
-            return new Response(null, {
-              // Temporary
-              status: 302,
-              headers: {
-                Location: `${pathname}/${search}`,
-              },
-            });
-          }
         }
       }
-    } catch (err) {
-      return new Response(`${err.message}\n\n${err.stack}`);
     }
 
     // Fall back to asset server

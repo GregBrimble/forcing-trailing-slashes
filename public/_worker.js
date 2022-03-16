71fd69db5d24ac2074c56ec6bfd59ecf8df8cf9a
Greg Brimble <gbrimble@cloudflare.com>
2022-03-16 23:35:21 +0000

    Magic

--- public/_worker.js
+++ public/_worker.js
@@ -1,13 +1,40 @@
 export default {
   async fetch(request, env) {
+    let { pathname, origin, search } = new URL(request.url);
+
+    if (pathname !== "/") {
+      if (!pathname.endsWith("/")) {
+        const assetUrlWithTrailingSlash = new URL(
+          `${pathname}/${search}`,
+          origin
+        );
+
+        assetRequest = new Request(assetUrlWithTrailingSlash, request);
+        assetRequest = new Request(assetRequest, {
+          headers: {
+            ...Object.fromEntries(request.headers.entries()),
+            "cf-worker": "user-worker", // Deactivates SPA mode
+          },
+        });
+
+        const assetResponse = await env.ASSETS.fetch(assetRequest);
+
+        if (
+          assetResponse.status === 200 &&
+          assetResponse.headers.get("content-type").includes("text/html")
+        ) {
+          return new Response(null, {
+            // Temporary
+            status: 302,
+            headers: {
+              Location: `${pathname}/${search}`,
+            },
+          });
+        }
+      }
+    }
+
     // Fall back to asset server
-    return env.ASSETS.fetch(
-      new Request(request, {
-        headers: {
-          ...Object.fromEntries(request.headers.entries()),
-          "cf-worker": "absolutely-not-set-from-the-internet",
-        },
-      })
-    );
+    return env.ASSETS.fetch(request);
   },
 };

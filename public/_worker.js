11b6694222f6706305fba70b6c71421a4ba03ab8
Greg Brimble <gbrimble@cloudflare.com>
2022-03-17 12:21:48 +0000

    :(

--- public/_worker.js
+++ public/_worker.js
@@ -3,16 +3,11 @@ export default {
     let { pathname, origin, search } = new URL(request.url);
 
     if (pathname.endsWith("/")) {
+      // Serve /foo.html at /foo/
+      // Serve /bar/index.html at /bar/
       return env.ASSETS.fetch(request, { redirect: "follow" });
     } else {
-      const assetUrlWithTrailingSlash = new URL(
-        `${pathname}/${search}`,
-        origin
-      );
-
-      let assetRequest = new Request(assetUrlWithTrailingSlash, request);
-      assetRequest = new Request(assetRequest, {
-        cf: { country: "foobar" },
+      const assetRequest = new Request(request, {
         headers: {
           ...Object.fromEntries(request.headers.entries()),
           "cf-worker": "deprecated-pages=disable-spa-mode",
@@ -27,14 +22,16 @@ export default {
         assetResponse.status === 200 &&
         assetResponse.headers.get("content-type").includes("text/html")
       ) {
+        // If an HTML asset can eventually be served at the URL with a trailing slash, redirect to the URL with a trailing slash.
         return new Response(null, {
-          // Temporary
           status: 302,
           headers: {
             Location: `${pathname}/${search}`,
           },
         });
       } else {
+        // Serve /bin at /bin as normal
+        // Serve /non-existent with normal 404 behavior (404 /404.html if available or 200 /index.html if not)
         return env.ASSETS.fetch(request);
       }
     }

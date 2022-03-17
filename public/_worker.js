f41584f8d0143338e8ca86560df5381980fb0eab
Greg Brimble <gbrimble@cloudflare.com>
2022-03-17 11:17:18 +0000

    conditionally serve asset redirect

--- public/_worker.js
+++ public/_worker.js
@@ -2,41 +2,40 @@ export default {
   async fetch(request, env) {
     let { pathname, origin, search } = new URL(request.url);
 
-    if (pathname !== "/") {
-      if (!pathname.endsWith("/")) {
-        const assetUrlWithTrailingSlash = new URL(
-          `${pathname}/${search}`,
-          origin
-        );
+    if (pathname.endsWith("/")) {
+      return env.ASSETS.fetch(request, { redirect: "follow" });
+    } else {
+      const assetUrlWithTrailingSlash = new URL(
+        `${pathname}/${search}`,
+        origin
+      );
 
-        let assetRequest = new Request(assetUrlWithTrailingSlash, request);
-        assetRequest = new Request(assetRequest, {
+      let assetRequest = new Request(assetUrlWithTrailingSlash, request);
+      assetRequest = new Request(assetRequest, {
+        headers: {
+          ...Object.fromEntries(request.headers.entries()),
+          "cf-worker": "user-worker", // Deactivates SPA mode
+        },
+      });
+
+      const assetResponse = await env.ASSETS.fetch(assetRequest, {
+        redirect: "follow",
+      });
+
+      if (
+        assetResponse.status === 200 &&
+        assetResponse.headers.get("content-type").includes("text/html")
+      ) {
+        return new Response(null, {
+          // Temporary
+          status: 302,
           headers: {
-            ...Object.fromEntries(request.headers.entries()),
-            "cf-worker": "user-worker", // Deactivates SPA mode
+            Location: `${pathname}/${search}`,
           },
         });
-
-        const assetResponse = await env.ASSETS.fetch(assetRequest, {
-          redirect: "follow",
-        });
-
-        if (
-          assetResponse.status === 200 &&
-          assetResponse.headers.get("content-type").includes("text/html")
-        ) {
-          return new Response(null, {
-            // Temporary
-            status: 302,
-            headers: {
-              Location: `${pathname}/${search}`,
-            },
-          });
-        }
+      } else {
+        return assetResponse;
       }
     }
-
-    // Fall back to asset server
-    return env.ASSETS.fetch(request, { redirect: "follow" });
   },
 };
